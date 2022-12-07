763da747a71d0d76c31b548e302e95a13c0efd28
Andrew Bingham <andrewgbingham@gmail.com>
2022-12-07 10:59:08 -0700

    call four methods and add descriptions

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,15 @@ test_scores = [99, 67, 78, 92]
 pocket_change = [5.25, 15.55, 1.00, 25.75]
 descriptions = [has_longhair, good_test_taker, is_funny, can_buy_dinner] 
 
+# This code will add "Buzz" element to friends_names variable in the last position
+friends_names.push "Buzz" 
+
+# This code will add an integer element to the test_scores variablein the first position and return it
+test_scores.unshift 100
+
+# This code will remove the last element in the pocket_change variable
+pocket_change.pop
+ 
+# This code will give the number of elements in the descriptions variable
+descriptions.length
+
