38136ba634576cbfc1c48b5323a59b9d85308eb5
Andrew Bingham <andrewgbingham@gmail.com>
2022-12-07 10:50:09 -0700

    define 4 variable arrays

---
+++ arrays.rb
@@ -0,0 +1,5 @@
+friends_names = ["John", "JA", "Brendan", "Zach"]
+test_scores = [99, 67, 78, 92]
+pocket_change = [5.25, 15.55, 1.00, 25.75]
+descriptions = [has_longhair, good_test_taker, is_funny, can_buy_dinner] 
+
